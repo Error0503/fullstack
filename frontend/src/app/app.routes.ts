6249c9642370770a7d44113b252038f5dca10b40
Vargha Simon <wfrh7n@inf.elte.hu>
2024-11-25 20:44:52 +0100

    Fixed folder structure

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -6,7 +6,7 @@ export const routes: Routes = [
     title: 'Login',
     path: 'login',
     loadChildren: () =>
-      import('./authentication/authentication/authentication.module').then(
+      import('./authentication/authentication.module').then(
         (m) => m.AuthenticationModule
       ),
     canActivate: [publicGuard],
--- frontend/src/app/authentication/authentication.module.ts
+++ frontend/src/app/authentication/authentication.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { LoginComponent } from '../../components/login/login.component';
+import { LoginComponent } from '../components/login/login.component';
 import { RouterModule, Routes } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 
