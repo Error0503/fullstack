9c8de1ad255775360349848add99122ae8f1a222
Balázs Martin <65673481+Phrodo02@users.noreply.github.com>
2025-01-21 02:02:58 +0100

    Revert "Add reporting functionality to BuildViewer component"

--- frontend/src/app/components/builds/build-viewer/build-viewer.component.ts
+++ frontend/src/app/components/builds/build-viewer/build-viewer.component.ts
@@ -10,18 +10,13 @@ import {
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
-import { CommonModule } from '@angular/common';
+import User from '../../../interfaces/user';
+import { Observable, Subject, tap } from 'rxjs';
 
-
-enum Reasons {
-  OFFENSIVE = 'offensive',
-  SPAM = 'spam',
-  MISLEADING = 'misleading',
-}
 @Component({
   selector: 'app-build-viewer',
   standalone: true,
-  imports: [RouterModule, CommonModule, ReactiveFormsModule],
+  imports: [RouterModule, ReactiveFormsModule],
   templateUrl: './build-viewer.component.html',
   styleUrl: './build-viewer.component.css',
 })
@@ -29,9 +24,7 @@ export class BuildViewerComponent {
   buildData: Build | undefined;
   loading: boolean = true;
   heroes = Object.values(heroesData);
-  reportForm: FormGroup;
-  reasons = Object.values(Reasons);
-  errorMessage?: string;
+
   userService: UserService;
 
   commentForm: FormGroup;
@@ -51,34 +44,17 @@ export class BuildViewerComponent {
     this.commentForm = this.formBuilder.group({
       content: ['', [Validators.required]],
     });
-    this.reportForm = this.fb.group({
-      body: ['', [Validators.required, Validators.minLength(10)]],
-      reason: [
-        'Select an option',
-        [
-          Validators.required,
-          Validators.pattern(Object.values(Reasons).join('|')),
-        ],
-      ],
-    });
   }
 
   private fetchData(id: number) {
     this.http.get(`http://localhost:3000/post/${id}`).subscribe({
       next: (data) => {
         this.buildData = JSON.parse(JSON.stringify({ ...data }));
+        console.log(this.buildData);
       },
       error: console.error,
       complete: () => (this.loading = false),
     });
-  }
-
-  get body() {
-    return this.reportForm.get('body');
-  }
-
-  get reason() {
-    return this.reportForm.get('reason');
   }
 
   deleteBuild() {
@@ -92,43 +68,6 @@ export class BuildViewerComponent {
       });
   }
 
-  reportBuild(e: any) {
-    e.preventDefault();
-    this.http
-      .post(`http://localhost:3000/report`, {
-        body: this.reportForm.value.body,
-        reason: this.reportForm.value.reason,
-        userId: this.userService.getUserId(),
-        postId: this.buildData?.id,
-      })
-      .subscribe({
-        error: (error) => {
-          if (error.error.statusCode === 500) {
-            this.errorMessage = 'Internal server error';
-          } else if (error.error.statusCode === 400) {
-            this.errorMessage = 'Invalid report';
-          } else {
-            this.errorMessage = 'An error occurred';
-          }
-        },
-
-        complete: () => {
-          this.reportForm.reset({
-            body: '',
-            reason: 'Select an option',
-          });
-          this.closeModal();
-          this.errorMessage = undefined;
-        },
-      });
-  }
-
-  closeModal() {
-    const modal = document.getElementById('my_modal_2') as HTMLDialogElement;
-    if (modal) {
-      modal.close();
-    }
-    
   saveComment() {
     this.http
       .post(`http://localhost:3000/comment`, {
